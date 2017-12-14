0149002de17e072465e110274968bbd7543bfee3
Xu Fei(Feilx) <xf21@vip.sina.com>
2017-12-15 01:40:16 +1300

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -5,8 +5,8 @@
 $(function(){
     if(navigator.userAgent.match(/MicroMessenger/i)){
         var weixinShareLogo = '';		
-        $('body').prepend('<div style=" overflow:hidden; width:0px; height:0; margin:0 auto; position:absolute; top:-800px;">
-                          <img src="'+ weixinShareLogo +'"></div>')	
+        $('body').prepend('<div style="overflow:hidden; width:0px; height:0; margin:0 auto; position:absolute; top:-800px;">
+             <img src="'+ weixinShareLogo +'"></div>')	
          };
         });
 
